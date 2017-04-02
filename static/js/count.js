8124784a910aa0b147a7b211d9259168e4e4b9e4
Kyle John <njk230@gmail.com>
2017-04-02 13:33:52 +0900

    Update counting system

--- static/js/count.js
+++ static/js/count.js
@@ -1,16 +1,4 @@
-function getVisitors() {
-    $.ajax({
-        url: 'https://countvisitor.azurewebsites.net/api/visitor/last',
-        type: 'get',
-        success: function (data) {
-            var p = document.getElementById('p');
-            if (data == null) p.innerText = '0';
-            else p.innerText = data.key;
-        }
-    });
-}
-
-function postVisitors() {
+$(document).ready(function () {
     $.ajax({
         url: 'https://countvisitor.azurewebsites.net/api/visitor',
         type: 'post',
@@ -24,27 +12,20 @@ function postVisitors() {
             console.log(JSON.stringify(data));
         }
     });
-}
 
-$(document).ready(function () {
     pre_content = $("#jalpc_site_pv").html();
     $("#jalpc_site_pv").html(pre_content);
-    var website_count = 0;
-    $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
-    // $.ajax({
-    //     type: "get",
-    //     async: false,
-    //     url: "https://vps.jarrekk.com:8443/api/v1.0/jalpc/pv_count",
-    //     dataType: "jsonp",
-    //     jsonp: "callback",
-    //     jsonpCallback: "flightHandler",
-    //     success: function (json) {
-    //         console.log("Count json: " + JSON.stringify(json));
-    //         var website_count = json.data;
-    //         $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
-    //     },
-    //     error: function () {
-    //         console.log('fail');
-    //     }
-    // });
+
+    $.ajax({
+        url: 'https://countvisitor.azurewebsites.net/api/visitor/last',
+        type: 'get',
+        success: function (json) {
+            var website_count;
+            
+            if (data == null) website_count = '0';
+            else website_count = json.key;
+
+            $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
+        }
+    });
 });
