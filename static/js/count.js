f31f9d250bfc8aa232d273bdf36146276b51f50b
Kyle John <njk230@gmail.com>
2017-04-02 12:45:28 +0900

    Test count.js

--- static/js/count.js
+++ static/js/count.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
+function getVisitors() {
+    $.ajax({
+        url: 'https://countvisitor.azurewebsites.net/api/visitor/last',
+        type: 'get',
+        success: function (data) {
+            var p = document.getElementById('p');
+            if (data == null) p.innerText = '0';
+            else p.innerText = data.key;
+        }
+    });
+}
+
+function postVisitors() {
+    $.ajax({
+        url: 'https://countvisitor.azurewebsites.net/api/visitor',
+        type: 'post',
+        contentType: 'application/json; charset=UTF-8',
+        dataType: 'json',
+        data: JSON.stringify({
+            referer: document.referrer,
+            ipaddress: ip()
+        }),
+        success: function (data) {
+            console.log(JSON.stringify(data));
+        }
+    });
+}
+
+$(document).ready(function () {
     pre_content = $("#jalpc_site_pv").html();
     $("#jalpc_site_pv").html(pre_content);
     $.ajax({
@@ -8,11 +36,12 @@ $(document).ready(function() {
         dataType: "jsonp",
         jsonp: "callback",
         jsonpCallback: "flightHandler",
-        success: function(json) {
+        success: function (json) {
+            console.log("Count json: " + JSON.stringify(json));
             var website_count = json.data;
             $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
         },
-        error: function() {
+        error: function () {
             console.log('fail');
         }
     });
